9fe452a9b63423730674fb568b3e9c0bb3621437
Shakir <shakir@evincedev.com>
2021-06-23 11:14:56 +0530

    First commit to demo repo

---
+++ DemoProject/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DemoProject
+//
+//  Created by Shakir Shaikh on 23/06/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DemoProject/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DemoProject
+//
+//  Created by Shakir Shaikh on 23/06/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
